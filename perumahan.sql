491f10b33600001b0f168d4896cd10f1c5e5fbbc
Irvan Fathul Huda <irvanfathul@gmail.com>
2018-05-16 13:26:42 +0700

    add sql

---
+++ perumahan.sql
@@ -0,0 +1,48 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.19 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.5.0.5278
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for perumahan
+CREATE DATABASE IF NOT EXISTS `perumahan` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `perumahan`;
+
+-- Dumping structure for table perumahan.afiliasis
+CREATE TABLE IF NOT EXISTS `afiliasis` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fk_user` int(10) unsigned NOT NULL,
+  `phone_ref` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_afi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fk_status_ref` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fk_status_afi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bonus_ref` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `redeem` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+-- Dumping structure for table perumahan.societe
+CREATE TABLE IF NOT EXISTS `societe` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status_client` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
